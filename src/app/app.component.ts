bee17d1ec8a6690f16cd4e354b12936d9451abcb
ApoRaviz <130882268+ApoRaviz@users.noreply.github.com>
2025-03-14 15:26:15 +0700

    sucsess

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,11 +7,17 @@ import { PixelgradeComponent } from './components/pixelgrade/pixelgrade.componen
 import { StatsComponent } from './components/stats/stats.component';
 import { HowComponent } from './components/how/how.component';
 import { MeetComponent } from './components/meet/meet.component';
+import { ArticleComponent } from './components/article/article.component';
+import { DemoComponent } from './components/demo/demo.component';
+import { FooterComponent } from './components/footer/footer.component';
+
 
 @Component({
   selector: 'app-root',
-  standalone : true,
-  imports: [NavbarComponent,HeaderComponent,ClientsComponent,CommunityComponent,PixelgradeComponent,StatsComponent,HowComponent,MeetComponent],
+  standalone: true,
+  imports: [NavbarComponent, HeaderComponent, ClientsComponent, CommunityComponent, PixelgradeComponent, StatsComponent, HowComponent, MeetComponent
+    , ArticleComponent,DemoComponent,FooterComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
